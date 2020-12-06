685023422dc09e60e13662c91ce681b3232c95c5
Margherita Pindaro <margherita.pindaro@studenti.unimi.it>
2020-12-06 11:37:53 +0100

    added day six

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,15 @@ function getInput(funzione){
 			fr.readAsText(this.files[0]);
 		})
 }
+
+let nav="<li><a href=\"day1.html\">Day 1 </a></li>\n" +
+	"\t<li><a href=\"day2.html\">Day 2 </a></li>\n" +
+	"\t<li><a href=\"day3.html\">Day 3 </a></li>\n" +
+	"\t<li><a href=\"day4.html\">Day 4 </a></li>\n" +
+	"\t<li><a href=\"day5.html\">Day 5 </a></li>\n" +
+	"\t<li><a href=\"day6.html\">Day 6 </a></li>"
+
+function setUpNav(mobile, nonmobile){
+	nonmobile.html(nav)
+	mobile.html(nav)
+}
